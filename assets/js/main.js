7ccaa01c821fc6ee16dd7d6e10824976303e0ba3
Renato Alves <devrenatoalves@gmail.com>
2024-03-07 23:16:42 -0300

    My first commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+function criaCalculadora () {
+    return {
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+        
+        inicia() {
+            this.cliqueBotoes()
+            this.pressionaEnter()
+            },
+        
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta()
+                }
+            })
+        },
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta)
+
+                if(!conta) {
+                    alert('Conta inválida')
+                    return
+                }
+
+                this.display.value = String(conta); 
+            } catch (e) {
+                alert('Conta inválida')
+                return;
+            }
+        },
+            
+        cliqueBotoes() {
+            document.addEventListener('click', (e) => { // Arrow function n altera o this
+                const el = e.target;
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText)
+                }
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay()
+                }
+
+                if(el.classList.contains('btn-del')) {
+                    this.apagaUm()
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta()
+                }
+
+                this.display.focus()
+            });
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor
+        }
+
+    }
+}
+
+const calculadora = criaCalculadora()
+calculadora.inicia()
